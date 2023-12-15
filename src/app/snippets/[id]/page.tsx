69573a5d2fe0a4889b79d5dcf1a76791d0773b8a
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-15 16:22:26 +0100

    styling the show page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -21,7 +21,19 @@ export default async function SnippetShowPage(props: any){
         }
     
     
-    return <div>{snippet.title}</div>;
+    return <div>
+        <div className='flex m-4 justify-between items-center'>
+            <h1 className='text m-l font-bold'>{snippet.title}</h1>
+
+            <div className="flex gap-4"> 
+                <button className='p-2 border rounded'>Edit</button>
+                <button className='p-2 border rounded'>Delete</button>
+            </div>
+            </div>
+            <pre className='p-3 border rounded bg-gray-500 border-gray-500'>
+                <code>{snippet.code}</code>
+            </pre>
+        </div>;
 
     
 }
